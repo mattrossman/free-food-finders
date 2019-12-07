4503d0a7634bfcdd7bd2093cacfd00fd58b0775e
mattrossman <mrossman@umass.edu>
2019-12-07 15:22:42 -0500

    Add updated create event form

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
+bool pressAttention = false;
 
 class MyApp extends StatelessWidget {
   @override
@@ -255,7 +256,119 @@ class AddEventButton extends StatelessWidget {
     );
   }
 }
-
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.star),
+              labelText: 'Name:'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return '*Missing Required Information';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.location_city),
+              labelText: 'Location:'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return '*Missing Required Information';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.event),
+              labelText: 'Date:'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return '*Missing Required Information';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.timer),
+              labelText: 'Time:'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return '*Missing Required Information';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.description),
+              labelText: 'Description:'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return '*Missing Required Information';
+              }
+              return null;
+            },
+          ),
+          RaisedButton(
+            child: new Text('Vegan'),
+            textColor: Colors.black,
+            shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(30.0),
+            ),
+          color: pressAttention ? Colors.white : Colors.green,
+          onPressed: () => setState(() => pressAttention = !pressAttention),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Created Event!')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 class AddEvent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -268,7 +381,7 @@ class AddEvent extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          child: Text('Go back!'),
+          child: MyCustomForm(),
         ),
       )
     );
