7c6045a5c770d2d6088334f36b462ed078c5215b
mattrossman <mrossman@umass.edu>
2019-12-07 18:13:30 -0500

    Center loading indicator

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class _MyAppState extends State<MyApp> {
               }
 
               // By default, show a loading spinner.
-              return CircularProgressIndicator();
+              return Center(child: CircularProgressIndicator());
             },
           ),
         ),
